c2da24ed4ddd694b917667b3cf382f49a3eb9047
Victor <victorstafy11@gmail.com>
2022-09-05 17:32:27 -0300

    post do status implementado, usuarios inativos por mais de 10 segundos sendo eliminados, tudo funcionando, aparentemente

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ server.post('/participants',async (req,res)=>{
             lastStatus: Date.now()
         })
 
-        await db.collection("message").insertOne({
+        await db.collection("messages").insertOne({
             from: user_profile.name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format("HH:MM:ss")
         })
         
@@ -85,25 +85,23 @@ server.post('/messages', async (req,res)=>{
 
     try{
         const current_user = req.headers.user;
-
+        
         if (validation.error) {
             res.status(422).send(validation.error.details);
             return;
         }
+        console.log('ola') 
+        const participants = await db.collection("participants").find().toArray();
 
-        const participant = await db.collection("participants").find().toArray();
-        console.log(participant)
+        const signed_user= participants.find((value) => value.name === current_user);
 
-        const signed_user= await db.collection("messages").findOne({
-            from:current_user,
-        })
-        
+        console.log(signed_user)
         if (!signed_user){
             res.sendStatus(422);
             return;
         }
 
-        await db.collection("message").insertOne({
+        await db.collection("messages").insertOne({
             from: current_user, to: user_msg.to, text: user_msg.text, type: user_msg.type, time: dayjs().format("HH:MM:ss")
         })
 
@@ -116,7 +114,7 @@ server.post('/messages', async (req,res)=>{
 
 server.get("/messages", async (req, res) => {
     const limit = parseInt(req.query.limit);
-    const current_user = req.headers.user;
+    const current_user = req.headers.user
   
     try {
       const msg = await db.collection("messages").find().toArray();
@@ -131,11 +129,59 @@ server.get("/messages", async (req, res) => {
         }
       });
       res.send(allowed_msg.slice(-limit));
-
     } catch {
       res.sendStatus(500);
     }
 });
+
+server.post('/status', async (req,res)=>{
+    const current_user = req.headers.user;
+    
+    try {
+
+        const signed_user= await db.collection("participants").findOne({
+            name:current_user,
+        })
+        
+        if (!signed_user){
+            res.sendStatus(404);
+            return;
+        }
+        
+        await db.collection("participants").updateOne(
+        {lastStatus: signed_user.lastStatus},
+        { $set: { lastStatus: Date.now() } }
+        );
+
+        setInterval( async ()=>{
+            const participants = await db.collection("participants").find().toArray();
+            const non_active_participants= participants.filter((value)=>
+                (value.lastStatus<Date.now()-10000)
+            )
+            non_active_participants.map(async (participant)=>{
+                
+                await db.collection("participants").deleteOne({lastStatus: participant.lastStatus,});
+                await db.collection("messages").insertOne({
+                from: participant.name,
+                to: "Todos",
+                text: "sai na sala...",
+                type: "status",
+                time: dayjs().format("HH:MM:ss"),
+                })
+            })
+            
+        },10000)
+        
+        res.sendStatus(200);
+    }
+    catch {
+        res.sendStatus(500);
+    }
+})
+
+// server.delete('/status', async (req, res) => {
+
+// });
 
 
 server.listen(5000,function(){console.log('port 5000')});
